547c1e68aa774847b3a34d4b2daa672d86086c4f
elliot sad <bamdadmahdi05@gmail.com>
2023-04-07 00:40:12 +0200

    vercel deploy

--- backend/app.js
+++ backend/app.js
@@ -1,22 +1,29 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const port = 4001
-const auth_router = require('./routes/auth.router')
-const mongoose = require('mongoose')
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = 4001;
+const auth_router = require("./routes/auth.router");
+const mongoose = require("mongoose");
 
-//middleware 
+//middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
-//database connect 
-mongoose.connect('mongodb+srv://arshiyah53:46646659@cluster0.p7fvwrc.mongodb.net/user?retryWrites=true&w=majority').then(()=>{
-    console.log("connected to db")
-})
+//database connect
+mongoose
+  .connect(
+    "mongodb+srv://arshiyah53:46646659@cluster0.p7fvwrc.mongodb.net/user?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("connected to db");
+  });
 
-//auth router                 
+//auth router
 app.use("/api/auth", auth_router);
 
+app.get("/", (req, res) => {
+  res.send("hello arshiya and mehdi");
+});
 
-app.listen(port, () => console.log(`listening on port ${port}!`))
+module.exprots = app;
